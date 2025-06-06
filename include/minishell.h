1deed579c89e4d68cea8d4320373474363e46b5a
ahmedbnk <benkrararara2@gmail.com>
2025-06-06 16:16:54 +0100

    commited

--- include/minishell.h
+++ include/minishell.h
@@ -8,12 +8,12 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
-#include <sys/wait.h>
-#include <errno.h>
+# include <sys/wait.h>
+# include <errno.h>
 
 # define single_q 39
 # define double_q 34
-#define BUFFER_SIZE 40
+# define BUFFER_SIZE 40
 
 typedef struct s_expand
 {
@@ -75,79 +75,72 @@ int					is_space(char c);
 int					are_they_equal(const char *str1, const char *str2);
 
 char	**customized_split(char const *s);
-//char        *expand_if_possible(char *string);
 char	*expand_if_possible(char *str, int g);
 int					should_i_expand(char *str, int index);
 int					is_expand_separator(char c);
 char				**split_for_expantion(char const *s);
 char				*custom_join(char const *s1, char const *s2);
 
-
-void	allocat_and_init(t_expand **expand_list, int how_much_to_expand, int heredoc_flag);
+void				allocat_and_init(t_expand **expand_list, int how_much_to_expand, int heredoc_flag);
 char				*new_str_after_expand(t_expand *data, int num_of_expantion);
-void remove_quotes_from_args(char **splitted);
-char	**split_with_operators(char **splitted);
-void print_splitted(char **splitted);
-t_data *make_token(char **arr);
-int len_of_two_d_array(char **str);
-void execute_command_line(t_shell_control_block *shell);
-
+void				remove_quotes_from_args(char **splitted);
+char				**split_with_operators(char **splitted);
+void				print_splitted(char **splitted);
+t_data				*make_token(char **arr);
+int					len_of_two_d_array(char **str);
+void				execute_command_line(t_shell_control_block *shell);
 
 int					ft_isalnum(int c);
 size_t				ft_strlen(const char *s);
 void				*ft_memcpy(void *dest, const void *src, size_t n);
 char				*ft_substr(char const *s, unsigned int start, size_t len);
 char				*ft_strjoin(const char *s1, const char *s2);
-// char				*ft_strdup(const char *s);
-char	*ft_strdup(const char *s, int flag);
-char	*ft_strchr(const char *s, int c);
-char	**ft_split(char const *s, char c);
-char	*ft_itoa(int n);
+char				*ft_strdup(const char *s, int flag);
+char				*ft_strchr(const char *s, int c);
+char				**ft_split(char const *s, char c);
+char				*ft_itoa(int n);
 
 t_list				*ft_lstnew(void *content);
 void				ft_lstadd_back(t_list **lst, t_list *new);
-void	*ft_malloc(size_t size, int falg);
+void				*ft_malloc(size_t size, int falg);
 t_list	**get_garbage_pointer(int flag);
-void	free_memory(t_list **lst);
-t_list	*garbage_collection_lstnew(void *content, int flag);
-// t_list	*garbage_collection_lstnew(void *content);
-char	*get_next_line(int fd);
-void handle_heredoc(t_data *tokenized, char **in_file_name);
-void print_file(char *str);
-void handle_redir_in(char *str, char **in_file_name);
-char *read_file(char *file_name);
-void handle_redir_out(char *str, char **file_name);
-void handle_append(char *str, char **file_name);
-char **get_cmd_and_its_args(t_shell_control_block *shell);
-void execute_command(t_shell_control_block *shell);
-void	remove_quotes(char **line);
+void				free_memory(t_list **lst);
+t_list				*garbage_collection_lstnew(void *content, int flag);
+char				*get_next_line(int fd);
+void				handle_heredoc(t_data *tokenized, char **in_file_name);
+void				print_file(char *str);
+void				handle_redir_in(char *str, char **in_file_name);
+char				*read_file(char *file_name);
+void				handle_redir_out(char *str, char **file_name);
+void				handle_append(char *str, char **file_name);
+char				**get_cmd_and_its_args(t_shell_control_block *shell);
+void				execute_command(t_shell_control_block *shell);
+void				remove_quotes(char **line);
 
-void	handle_signals_in_child(void);
-char	*generate_random_name(void);
+void				handle_signals_in_child(void);
+char				*generate_random_name(void);
 
-char ** copy_env(char **env);
-void print_env(char **env);
-void echo(char **args);
-int	ft_strncmp(const char *big, const char *little, size_t n);
-char *pwd();
-int execute_built_in(t_shell_control_block *shell);
-void print_env(char **env);
-void export(char ***env, char **to_export);
-int is_it_key_value(char *str);
-int is_valid_var(char *str);
-int compare_env_var(char *var1, char *var2);
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_strcmp(char *s1, char *s2);
+char				**copy_env(char **env);
+void				print_env(char **env);
+void				echo(char **args);
+int					ft_strncmp(const char *big, const char *little, size_t n);
+char				*pwd();
+int					execute_built_in(t_shell_control_block *shell);
+void				print_env(char **env);
+void				export(char ***env, char **to_export);
+int					is_it_key_value(char *str);
+int					is_valid_var(char *str);
+int					compare_env_var(char *var1, char *var2);
+int					ft_isalpha(int c);
+int					ft_isdigit(int c);
+int					ft_strcmp(char *s1, char *s2);
 
+char				**handle_dollar_with_quotes(char **splitted);
 
-char **handle_dollar_with_quotes(char **splitted);
-
-//int	ft_strncmp(const char *big, const char *little, size_t n);
-void cd(char **env, char **path);
-int	print_error(const char *str, ...);
-void create_all_heredocs(t_data *tokenized);
-void unset(char ***env, char **vars);
+void				cd(char **env, char **path);
+int					print_error(const char *str, ...);
+void				create_all_heredocs(t_data *tokenized);
+void				unset(char ***env, char **vars);
 
 // Global shell pointer for accessing shell state
 extern t_shell_control_block *g_shell;
