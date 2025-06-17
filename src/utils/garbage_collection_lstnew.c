00e4ed7d194a9737dcfa1b1e18d8693aa0e71cc2
ahmedbnk <benkrararara2@gmail.com>
2025-06-18 00:35:23 +0100

    commited

--- src/utils/garbage_collection_lstnew.c
+++ src/utils/garbage_collection_lstnew.c
@@ -4,6 +4,7 @@ t_list	*garbage_collection_lstnew(void *content, int flag)
 {
 	t_list	*node;
 
+	(void)flag; // Suppress unused parameter warning
 	node = malloc(sizeof(t_list));
 	if(!node)
 		return NULL;
