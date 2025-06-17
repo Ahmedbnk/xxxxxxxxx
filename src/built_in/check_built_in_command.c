4145dd7af9fb4347bde43b31249c6dd1a40306ef
ahmedbnk <benkrararara2@gmail.com>
2025-06-17 02:53:05 +0100

    committed

--- src/built_in/check_built_in_command.c
+++ src/built_in/check_built_in_command.c
@@ -12,7 +12,12 @@ int  execute_built_in(t_shell_control_block *shell)
     else if(are_they_equal(*shell->cmd_and_args, "cd"))
       return ((cd(shell->env_cpy, shell->cmd_and_args, 0), 1));
     else if(are_they_equal(*shell->cmd_and_args, "export"))
-      return((export(&shell->env_cpy, shell->cmd_and_args +1),1));
+    {
+      printf("DEBUG: execute_built_in: before export: env_cpy = %p\n", (void*)shell->env_cpy);
+      export(&shell->env_cpy, shell->cmd_and_args +1);
+      printf("DEBUG: execute_built_in: after export: env_cpy = %p\n", (void*)shell->env_cpy);
+      return 1;
+    }
     else if(are_they_equal(*shell->cmd_and_args, "unset"))
       return((unset(&shell->env_cpy, shell->cmd_and_args +1),1));
   return 0;
