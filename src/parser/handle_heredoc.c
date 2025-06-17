33088d3bb8028b50f95c8cd6f6b11ac4425ab3d0
ahmedbnk <benkrararara2@gmail.com>
2025-06-17 19:41:00 +0100

    commited

--- src/parser/handle_heredoc.c
+++ src/parser/handle_heredoc.c
@@ -37,9 +37,6 @@ void create_heredoc(t_shell_control_block *s ,t_token *tokenized)
   tokenized->heredoc_file_name = ft_strjoin("/tmp/", generate_random_name());
   tokenized->delimiter = remake_delimeter((tokenized + 1) -> word);
   
-  // Debug: print the delimiter
-  printf("DEBUG: delimiter = '%s'\n", tokenized->delimiter);
-  
   while(1)
   {
     str = readline(">");
@@ -55,9 +52,6 @@ void create_heredoc(t_shell_control_block *s ,t_token *tokenized)
     // Expand for content (but not for delimiter comparison)
     str = expand_if_possible(s, str, 1);
     
-    // Debug: print the input and comparison
-    printf("DEBUG: original input = '%s', delimiter = '%s', equal = %d\n", original_str, tokenized->delimiter, are_they_equal(original_str, tokenized->delimiter));
-    
     if(are_they_equal(original_str, tokenized->delimiter))
     {
       free(original_str);
@@ -66,7 +60,12 @@ void create_heredoc(t_shell_control_block *s ,t_token *tokenized)
     
     // Use expanded string for content
     if (str && *str)
-      buffer = ft_strjoin(buffer, str);
+    {
+      if (!buffer)
+        buffer = ft_strdup(str, 1);
+      else
+        buffer = ft_strjoin(buffer, str);
+    }
     free(original_str);
   }
   
