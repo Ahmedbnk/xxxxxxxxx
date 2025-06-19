9ea02dc3d359ead33c938c7cbd9bc238abdce353
ahmedbnk <benkrararara2@gmail.com>
2025-06-19 01:39:45 +0100

    commited

--- src/parser/handle_append.c
+++ src/parser/handle_append.c
@@ -4,6 +4,13 @@ void handle_append(char *str, char **file_name)
 {
   int fd;
 
+  // Don't create a file if the filename is the EMPTY_REDIR marker
+  if (are_they_equal(str, "EMPTY_REDIR"))
+  {
+    *file_name = NULL;
+    return;
+  }
+
   *file_name = str;
   fd = open(str, O_CREAT| O_RDWR | O_APPEND, 0644);
   close(fd);
